67c3aed8baf684691a08b318863a75c5f1305c64
Tazmilur20 <tazmilurrahmanodri@gmail.com>
2021-09-19 08:49:03 +0600

    component commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products=[
+    {name:'Photoshop',price:'$99.99'},
+    {name:'Illustrator', price:'$89.99'},
+    {name:'Adobe', price:'6.99'}
+      ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Product product={products[0]}></Product>
+      <Product product={products[1]}></Product>
+      <Product product={products[2]}></Product>
+        {/* <Product name={products[0].name} price={products[0].price}></Product>
+        <Product name={products[1].name} price={products[1].price}></Product>
+        <Product name={products[2].name} price={products[2].price}></Product> */}
+        <Person name="tazmilur" job=" engr."></Person>
+        <Person name="roni" job=" business"></Person>
+        
+        
       </header>
     </div>
   );
+}
+function Person(props){
+  return(
+    <div style={{border:'2px solid gold', width:'400px',padding:'20px'}}>
+       <h1>Name: {props.name}</h1>
+       <p> profession:{props.job}</p>
+    </div>
+  )
+}
+function Product(props){
+  const productStyle={
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'purple',
+    height:'200px',
+    width:'200px',
+    float:'left'
+
+  }
+  const{name,price}=props.product;
+  return(
+    <div style={productStyle}>
+      <h2>{name}</h2>
+      <h1>{price}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
 }
 
 export default App;
